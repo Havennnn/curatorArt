1df980ee62567c45b3568346fca4a590ac69a9c8
Havennnn <latrellmarable00@gmail.com>
2025-02-06 09:53:47 +0800

    Vercel fix #2

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
-/* import About from "./components/About.jsx"; */
-import Hero from "./components/Hero.jsx";
-import Divider from "./components/Divider.jsx";
-import Navbar from "./components/Navbar.jsx";
-import Projects from "./components/Projects.jsx";
-import History from "./components/History.jsx";
-import Events from "./components/Events.jsx";
-import Footer from "./components/Footer.jsx";
+import About from "./Components/About.jsx";
+import Hero from "./Components/Hero.jsx";
+import Divider from "./Components/Divider.jsx";
+import Navbar from "./Components/Navbar.jsx";
+import Projects from "./Components/Projects.jsx";
+import History from "./Components/History.jsx";
+import Events from "./Components/Events.jsx";
+import Footer from "./Components/Footer.jsx";
 
 const App = () => {
   return (
@@ -15,7 +15,7 @@ const App = () => {
       <Divider />
       <History />
       <Events />
-      {/* <About /> */}
+      <About />
       <Projects />
       <Footer />
     </>
