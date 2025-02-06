01c54b571b94f967cdd2925daf0b7e3c181d4a9b
Havennnn <latrellmarable00@gmail.com>
2025-02-06 09:49:15 +0800

    app fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
-import About from "./components/About";
-import Hero from "./components/Hero";
-import Divider from "./components/Divider";
-import Navbar from "./components/Navbar";
-import Projects from "./components/Projects";
-import History from "./components/History";
-import Events from "./components/Events";
-import Footer from "./components/Footer";
+import About from "./components/About.jsx";
+import Hero from "./components/Hero.jsx";
+import Divider from "./components/Divider.jsx";
+import Navbar from "./components/Navbar.jsx";
+import Projects from "./components/Projects.jsx";
+import History from "./components/History.jsx";
+import Events from "./components/Events.jsx";
+import Footer from "./components/Footer.jsx";
 
 const App = () => {
   return (
--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,19 +1,29 @@
-import { FaGithubSquare, FaLinkedin, FaPhoneAlt, FaEnvelope } from 'react-icons/fa';
+import {
+  FaGithubSquare,
+  FaLinkedin,
+  FaPhoneAlt,
+  FaEnvelope,
+} from "react-icons/fa";
 
 const Footer = () => {
   return (
-    <div className='bg-[#0C1830] py-10'>
-      <div className='align-element grid md:grid-cols-2 items-center gap-8'>
-        <article className='flex flex-col gap-y-2'>
-                <p className='text-white font-bold'>Contact Me</p>
-                <a className='flex flex-row gap-2 text-white'><FaEnvelope className='h-7 w-7 text-white' /> marablelatrell@gmail.com</a>
-                <a className='flex flex-row gap-2 text-white'><FaPhoneAlt className='h-7 w-7 text-white' /> 09953324941</a>
+    <div className="bg-base py-10">
+      <div className="align-element grid md:grid-cols-2 items-center gap-8">
+        <article className="flex flex-col gap-y-2">
+          <p className="text-white font-bold">Contact Me</p>
+          <a className="flex flex-row gap-2 text-white">
+            <FaEnvelope className="h-7 w-7 text-white" />{" "}
+            marablelatrell@gmail.com
+          </a>
+          <a className="flex flex-row gap-2 text-white">
+            <FaPhoneAlt className="h-7 w-7 text-white" /> 09953324941
+          </a>
         </article>
-        <article className='block'>
-            <div className='flex gap-x-5 md:float-right '>
-                <FaGithubSquare className='h-10 w-10 text-white hover:text-black duration-300 hover:cursor-pointer' />
-                <FaLinkedin className='h-10 w-10 text-white hover:text-black duration-300 hover:cursor-pointer' />
-            </div>
+        <article className="block">
+          <div className="flex gap-x-5 md:float-right ">
+            <FaGithubSquare className="h-10 w-10 text-white hover:text-black duration-300 hover:cursor-pointer" />
+            <FaLinkedin className="h-10 w-10 text-white hover:text-black duration-300 hover:cursor-pointer" />
+          </div>
         </article>
       </div>
     </div>
